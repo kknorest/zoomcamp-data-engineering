c1ac115eab9e1954184ae28753e4d99dafa2e39b
kknorest <jinhua.ding@mail.mcgill.ca>
2026-01-25 22:24:24 +0000

    homework 1

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from zoomcamp-data-engineering!")
+
+
+if __name__ == "__main__":
+    main()
--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -72,26 +73,26 @@ def ingest_data(
 
     print(f'done ingesting to {target_table}')
 
-def main():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    year = 2021
-    month = 1
-    chunksize = 100000
-    target_table = 'yellow_taxi_data'
-
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of data to ingest')
+@click.option('--month', default=1, type=int, help='Month of data to ingest')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+@click.option('--table', default='yellow_taxi_data', help='Target table name')
+@click.option('--url-prefix', default='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow', help='URL prefix for data files')
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, table, url_prefix):
+    """Ingest NYC taxi data into PostgreSQL database."""
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-    url_prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-
     url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
 
     ingest_data(
         url=url,
         engine=engine,
-        target_table=target_table,
+        target_table=table,
         chunksize=chunksize
     )
 
